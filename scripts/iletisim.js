2281ced87af483c1ef8a83fc65143eea004042d0
emine <emine.boluk@ogr.sakarya.edu.tr>
2024-05-19 22:15:09 +0300

    16

--- scripts/iletisim.js
+++ scripts/iletisim.js
@@ -6,14 +6,17 @@ var app = new Vue({
       radio2: '',
       date: '',
       mailFormat: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-      rangeValue: ''
+      rangeValue: '',
+      mesaj: ''
     },
     methods: {
       updateValue: function(val) {
         this.rangeValue = val;
       },
       checkForm: function() {
-        if (this.isim == "" || this.mail == "" || this.radio2 == "" || this.date == "") {
+
+        
+        if (this.isim == "" || this.mail == "" || this.radio2 == "" || this.date == ""|| this.mesaj == "") {
           alert("Lütfen tüm alanları doldurunuz.vue");
           return false;
         }
@@ -22,8 +25,34 @@ var app = new Vue({
           alert("Lütfen geçerli bir e-mail adresi giriniz.vue");
           return false;
         }
-  
-        window.location.href = 'iletisim.php';
+       
       }
     }
   });
+
+  e.preventDefault();
+  
+ 
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", 'iletisim.php', true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+
+  var data = "isim=" + this.isim + "&mail=" + this.mail + "&radio2=" + this.radio2 + "&date=" + this.date + "&mesaj=" + this.mesaj;
+
+
+  xhr.send(data);
+
+ 
+  xhr.onload = function() {
+    if (xhr.status == 200) {
+   
+      window.location.href = 'iletisim.php';
+    } else {
+    
+      alert("Bir hata oluştu: " + xhr.status);
+    }
+  }
+
+  
+
